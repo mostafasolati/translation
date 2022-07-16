b2dc7d64c452b133ce542f00f718e55418243454
Mostafa Solati <mostafa.solati@gmail.com>
2022-07-16 23:42:15 +0430

    second

--- translation.go
+++ translation.go
@@ -5,7 +5,7 @@ import (
 	"github.com/mostafasolati/translation/persian"
 )
 
-func TranslateWeekDay(lang string, day int) string {
+func TranslateWeekDay2(lang string, day int) string {
 	switch lang {
 	case "fa":
 		return persian.TranslateWeekDay(day)
